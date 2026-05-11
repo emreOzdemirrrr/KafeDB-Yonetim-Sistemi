46b1d7a5fbbe9fca94baba6da10a33565ca7ae9f
Emre Özdemir <emreozdemirr76@gmail.com>
2026-05-11 10:52:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,16 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import pyodbc
 
 app = Flask(__name__)
-app.secret_key = "super_gizli_anahtar"  # Uyarı mesajları verebilmek için gerekli
+app.secret_key = "super_gizli_anahtar"  
 
 
-# --- VERİTABANI BAĞLANTISI ---
 def baglanti_kur():
-    # DİKKAT: Buradaki YOUR_SERVER_NAME kısmına SSMS'teki kendi sunucu adını yaz!
     server = r'HackerReis'
     database = 'KafeDB'
     conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
     return pyodbc.connect(conn_str)
 
 
-# --- ANA SAYFA (ÜRÜNLERİ LİSTELE) ---
 @app.route('/')
 def index():
     conn = baglanti_kur()
@@ -25,7 +22,6 @@ def index():
     return render_template('index.html', urunler=urunler)
 
 
-# --- KATEGORİ EKLE (STORED PROCEDURE ÇAĞIRMA) ---
 @app.route('/kategori_ekle', methods=['POST'])
 def kategori_ekle():
     ad = request.form['kat_ad']
@@ -34,7 +30,6 @@ def kategori_ekle():
     conn = baglanti_kur()
     try:
         cursor = conn.cursor()
-        # Yazdığın Stored Procedure'ü çalıştırıyoruz
         cursor.execute("{CALL sp_KategoriEkle (?, ?)}", (ad, aciklama))
         conn.commit()
         flash(f"'{ad}' kategorisi başarıyla eklendi!", "success")
